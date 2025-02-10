eaa7cce900a181ea825edf2b2acc5b4fccaf6265
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-02-10 18:37:11 +0300

    Update Documentation.js

--- Documentation.js
+++ Documentation.js
@@ -1,4 +1,4 @@
-Источники:
+Источники знаний:
 https://learn.javascript.ru/
 https://scriptdev.ru/ 
 
@@ -6,3 +6,9 @@ https://scriptdev.ru/
 Тренажёры:
 https://www.codewars.com/collections/javascript-basics-2
 https://leetcode.com/studyplan/30-days-of-javascript/
+
+---
+В помощь к резюме:
+https://buildin.ai/share/ee13046b-da1c-4647-b986-08fec57f39f3?code=UMEHJC#d73dfb10-5084-43db-938a-f42eae433fa8
+
+
