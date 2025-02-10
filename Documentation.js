86cdd85013af650f6f5423c5b955fcfcc0bbf8f1
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-02-10 19:57:46 +0300

    Update Documentation.js

--- Documentation.js
+++ Documentation.js
@@ -4,12 +4,28 @@ https://scriptdev.ru/
 
 ---
 Тренажёры:
+https://codepen.io/trending 
 https://www.codewars.com/collections/javascript-basics-2
 https://leetcode.com/studyplan/30-days-of-javascript/
 https://leetcode.com/problems/binary-search/description/
 
 ---
 В помощь к резюме:
+https://hh.ru/resume/eef05344ff0e31871f0039ed1f453551483930
 https://buildin.ai/share/ee13046b-da1c-4647-b986-08fec57f39f3?code=UMEHJC#d73dfb10-5084-43db-938a-f42eae433fa8
+https://easyoffer.ru/rating/frontend_developer
+https://getmatch.ru/vacancies/20299?s=applications_status
+
+--- 
+Сервера:
+https://my.selectel.ru/balance/add
+
+--- 
+Беспл. Курсы:
+https://education.yandex.ru/handbook/algorithms
+
+---
+Зависимости В раб. проектах: 
+https://strapi.io/
 
 
