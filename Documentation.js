45143b230687511fb633be89fc5b54feec235ee5
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-02-10 18:36:03 +0300

    Create Documentation.js

---
+++ Documentation.js
@@ -0,0 +1,8 @@
+Источники:
+https://learn.javascript.ru/
+https://scriptdev.ru/ 
+
+---
+Тренажёры:
+https://www.codewars.com/collections/javascript-basics-2
+https://leetcode.com/studyplan/30-days-of-javascript/
