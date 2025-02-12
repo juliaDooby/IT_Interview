38c847bd8bf9067946d5d60d277fb09eaa07289e
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-02-12 20:14:44 +0300

    Create myResume.js

---
+++ Tools/myResume.js
@@ -0,0 +1,153 @@
+---
+Javascript Frontend разработчик(React,Redux,TypeScript,Git,Rest API,Next,HTML5,SCSS,Docker)
+Специализации:
+Программист, разработчик
+Занятость: полная занятость
+График работы: полный день, гибкий график, удаленная работа
+Желательное время в пути до работы: Не имеет значения
+готова к редким командировкам
+Рассматриваю предложения
+Контакты
+ +7 (905) 583-02-55 (Тел.регистрации на hh)
+Телефон подтвержден
++7 (963) 788-27-32 Основной Рабочий контакт/Telegram/WhatsApp: @juliaDooby — предпочитаемый способ связи
+juliadooby@yandex.ru
+https://github.com/juliaDooby
+Способы связи
+ LinkedIn
+ @juliaDooby
+
+Опыт работы 5 лет 1 месяц
+
+Июнь 2021 — по настоящее время
+3 года 9 месяцев
+Продуктовая компания "Dilligenforce Technology"
+ReactJS Frontend разработчик
+Работала над сайтом и мобильным приложением для агентов недвижимости с 500000
+аудиторией в месяц в продуктовой команде 15+ человек
+··················································································
+Достижения:
+- Расширила модуль функционалом для создания и архивирования задач и заявок по
+агентским договорам с возможностью загрузки документов
+- Настроила новый алгоритм для организации показов недвижимости и формирования
+агентских отчётов
+- Разработала и сопровождала функцию авторизации, регистрации и выхода пользователя из
+аккаунта (feature/logout)
+- Оптимизировала и поддерживала модуль оплаты подписок для агентов и партнёров
+- Рефакторила код и занималась ведением технической документации
+- Реализовала логику фильтрации и сортировки для выбора объектов, а также разработала
+страницу для связи с агентами
+- Добавила функции для оценки видео-обзоров объектов, сохранения их в избранное и
+создания персональных подборок для удобства агентов
+- Задействовала современный минималистичный интерфейс с помощью TailwindCSS,
+Material-UI, Styled Components, SCSS и React Hooks
+- Реализовала интеграцию с API и настроила авторизацию для безопасного доступа к нужной
+информации
+- Использовала MongoDB для работы со сложными структурами и множественным заполнением данных
+- Писала и поддерживала тесты (Unit, Snapshot, E2E), используя библиотеки React Testing Library,
+Jest, Cypress и Enzyme
+- Внесла вклад в разработку архитектуры приложения и помогала разрабатывать стратегию
+развития продукта
+- Настраивала CI/CD для автоматизации процессов сборки и развёртывания приложения
+Стек проекта: React, Next.js, JavaScript, Typescript, async/await, Redux, Redux Toolkit, Redux-Saga,
+react-router-dom, axios, Rest API, Git, React Native, Bootstrap, bootstrap-grid, Material-UI, Tailwind
+CSS, HTML5, CSS3, ESLint, Webpack, SCSS, styled-components
+Backend (базовый опыт): Node.js, ExpressJS, Nest.js, MongoDB, Postman, PostgreSQL
+
+Февраль 2020 — Июнь 2021
+1 год 5 месяцев
+Аутсорс компания "Веб-студия Webminds Studio.Pro"
+ReactJS Frontend разработчик
+Разрабатывала интерфейсы и функционал сайта «clideo.com», работала в команде с 3 frontend, 4
+qa, 5 backend
+··················································································
+Достижения:
+- Провела редизайн основных экранов с полным обновлением пользовательского потока и
+рефакторингом, а также привела разделы сайта к единому стилю через внедрение
+дизайн-системы
+- В проекте отвечала за главную страницу, оптимизировала флоу, скрыв неактуальные разделы
+для конкретного пользователя
+- Корректировала существующий функционал сайта и устраняла баги, включая контент и сбои
+- Организовала аутентификацию с использованием Clerk для удобного и безопасного доступа
+- Проводила код-ревью задач средней сложности
+Стек проекта: React, Javascript, Redux, Redux Toolkit, React-router-dom, Bootstrap-grid, HTML5,
+CSS3, SCSS, Styled-components, Webpack, Axios, REST API
+
+Ключевые навыки
+React
+JavaScript
+ES6+
+Async/Await
+TypeScript
+Redux
+Redux Toolkit
+Redux-Saga
+Next.js
+jQuery
+React-router
+React-router-dom
+MobX
+BEM
+HTML5
+CSS3
+SCSS
+Styled-components
+ESLint
+Bootstrap
+Bootstrap-grid
+Material-UI
+UIKit
+Tailwind CSS
+GSAP
+Figma
+Axios
+REST API
+Websockets
+Git
+
+Обо мне
+··················································································
+Языки программирования: Javascript
+Agile Methodology: Scrum, Kanban
+Архитектуры: Компонентная, Single Page Application (SPA), Flux/Redux, Design System / UI-Kit,
+Server-Side Rendering (SSR)
+Стек/Технологии: React, JavaScript, ES6+, Async/Await, Typescript, Redux, Redux Toolkit,
+Redux-Saga, Next.js, jQuery, React-router, React-router-dom, MobX, BEM, HTML5, CSS3, SCSS,
+Styled-components, ESLint, Bootstrap, Bootstrap-grid, Material-UI, UIKit, Tailwind CSS, GSAP,
+Figma, Axios, Rest API, Websockets, Git, Webpack, SOLID, OOP, UI/Unit test, Scrum, Kanban, React
+Native, Node.js, Nest.js, ExpressJS, Docker, JWT, CI/CD, MongoDB, Postman, PostgreSQL,
+Английский язык
+········
+Бэкграунд:
+- Уточняла и документировала проектные требования на всех этапах разработки
+- Разбивала задачи на подзадачи для улучшения планирования и контроля выполнения
+- Координировала работу с менеджерами, тестировщиками и backend-командой для
+решения задач в срок
+- Осуществляла менторскую поддержку и онбординг новых членов команды
+- Сотрудничала с командой тестировщиков в поиске и анализе ошибок в продакшен-версии
+- Помогала команде в релизе и запуске сайтов
+········
+Мои проекты (Github репозиторий):
+- Сайт для приема заказов на доставку еды и навынос
+[React-food-delivery-app](https://github.com/juliaDooby/React-food-delivery-app)
+[Demo](https://juliadooby.github.io/React-food-delivery-app/)
+- Cервис микроблогинга и социальная сеть
+[React-threads-app](https://github.com/juliaDooby/React-threads-app)
+[Demo](Устранение серверной ошибки)
+- Шахматы
+[React-chess-app](https://github.com/juliaDooby/React-chess)
+[Demo](https://juliadooby.github.io/React-chess/)
+········
+Контакты:
+Telegram/WhatsApp: @juliaDooby
+Skype: @juliaDooby
+Linkedin: www.linkedin.com/in/juliadooby
+Habr: https://career.habr.com/juliadooby
+Email: juliadooby@yandex.ru
+CV: https://juliadooby.github.io/CV/
+Github·page: https://github.com/juliaDooby
+Website: juliadooby.github.io/Portfolio/
+Высшее образование
+2013
+Государственный университет просвещения (экс Московский государственный областной университет (МГОУ)), Москва
+Лингвистический факультет, Перевод и переводоведение
