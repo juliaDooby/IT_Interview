1e507dd3a71fc5a87578a1688bc1a551145c0d5c
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-02-16 00:19:23 +0300

    Update Note-Documentation.js

--- Tools/Note-Documentation.js
+++ Tools/Note-Documentation.js
@@ -4,6 +4,7 @@ deplot fullstack приложения 2025
 Деплой Full-Stack приложения | Полный гайд 2025
 Github Actions - Введение в CI/CD минин 
 🚀 Деплой Fullstack Приложения [Golang+JS]. СI/CD
+https://www.youtube.com/watch?v=ceRTzKuCLnw
  
 ---
 Ещё таски:
