982133bf77527cafd3e01f985c51617dbf88aa7c
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-02-17 19:51:23 +0300

    Update Note-Documentation.js

--- Tools/Note-Documentation.js
+++ Tools/Note-Documentation.js
@@ -3,6 +3,7 @@
 deploy fullstack приложения 2025
 Деплой Full-Stack приложения | Полный гайд 2025
 Github Actions - Введение в CI/CD минин 
+https://genius.courses/%D0%BA%D0%B0%D0%BA-%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C-%D0%B4%D0%B5%D0%BF%D0%BB%D0%BE%D0%B9-%D1%81%D0%B0%D0%B9%D1%82%D0%B0-%D1%87%D0%B5%D1%80%D0%B5%D0%B7-github-actions/
 🚀 Деплой Fullstack Приложения [Golang+JS]. СI/CD
 https://www.youtube.com/watch?v=ceRTzKuCLnw
 🚀 Деплой Fullstack Приложения бесплатно 
@@ -10,6 +11,9 @@ https://www.youtube.com/results?search_query=%F0%9F%9A%80+%D0%94%D0%B5%D0%BF%D0%
 Деплой Frontend приложения. Настройка nginx. Подключаем домен, настраиваем HTTPS, gzip, docker 
 Ulbi TV
 https://www.youtube.com/watch?v=8OHe6chCWTE&t=543s
+https://www.google.com/search?q=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DuLp-zgset00&oq=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DuLp-zgset00&gs_lcrp=EgZjaHJvbWUyBggAEEUYQTIGCAEQRRg5MgYIAhBFGDzSAQgxMzcxajBqNKgCALACAQ&sourceid=chrome&ie=UTF-8
+https://www.google.com/search?q=%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE+%D0%BB%D0%B8+fullstack+%D1%81%D0%B0%D0%B9%D1%82+%D0%B7%D0%B0%D0%BF%D1%83%D1%81%D1%82%D0%B8%D1%82%D1%8C+%D0%B2+github+actions&sca_esv=6f9618c19bdd3933&sxsrf=ADLYWIIfPduEMfsaK4ROqp7fvP2zLHjQCg%3A1735609079107&ei=90pzZ9eVBuDaptQPkeDP2AI&ved=0ahUKEwiX7YPV79CKAxVgrYkEHRHwEysQ4dUDCBA&uact=5&oq=%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE+%D0%BB%D0%B8+fullstack+%D1%81%D0%B0%D0%B9%D1%82+%D0%B7%D0%B0%D0%BF%D1%83%D1%81%D1%82%D0%B8%D1%82%D1%8C+%D0%B2+github+actions&gs_lp=Egxnd3Mtd2l6LXNlcnAiR9C80L7QttC90L4g0LvQuCBmdWxsc3RhY2sg0YHQsNC50YIg0LfQsNC_0YPRgdGC0LjRgtGMINCyIGdpdGh1YiBhY3Rpb25zMgUQABjvBTIFEAAY7wUyCBAAGIAEGKIEMgUQABjvBTIFEAAY7wVIpw1QugZYugZwAXgBkAEAmAHxBKAB8QSqAQM1LTG4AQPIAQD4AQGYAgKgAoQFwgIKEAAYsAMY1gQYR5gDAIgGAZAGBJIHBTEuNS0xoAfEAw&sclient=gws-wiz-serp#fpstate=ive&vld=cid:3f41c862,vid:icbBxY2BaiE,st:0
+https://habr.com/ru/companies/selectel/articles/836228/
 ---
 Ещё таски:
 https://habr.com/ru/articles/871792/
@@ -18,9 +22,14 @@ https://habr.com/ru/articles/871792/
 Источники знаний/Тех Документация:
 https://liquidhub.ru/blogs/blog/kak-bystro-verstat
 https://developer.mozilla.org/ru/docs/Learn_web_development
+https://developer.mozilla.org/ru/docs/Learn/JavaScript
 https://learn.javascript.ru/
 https://javascript.ru/
 https://javascript.info/
+https://habr.com/ru/companies/simbirsoft/articles/652321/
+https://it-dev-journal.ru/articles/kak-bystro-ponyat-huk-use-effect-v-react
+https://ru.legacy.reactjs.org/docs/hooks-effect.html
+https://webtricks-master.ru/react-hooks/uchim-useeffect-na-primerah/
 https://www.google.com/search?q=javascript+%D0%B4%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F&sca_esv=6b5ec6d4658d14f9&sxsrf=ADLYWIJTKrfi22p14RwUgzlf7gmddo23jg%3A1734895884476&ei=DGloZ8ndHIWkiLMPl9Wl-AY&ved=0ahUKEwjJk6rnjryKAxUFEmIAHZdqCW8Q4dUDCBE&uact=5&oq=javascript+%D0%B4%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F&gs_lp=Egxnd3Mtd2l6LXNlcnAiI2phdmFzY3JpcHQg0LTQvtC60YPQvNC10L3RgtCw0YbQuNGPMgUQABiABDIGEAAYFhgeMgYQABgWGB4yCBAAGIAEGKIEMggQABiABBiiBDIFEAAY7wUyBRAAGO8FSJc4UABYmjVwAHgBkAEBmAGpBaAB4TCqAQoyLTE1LjMuMC4zuAEDyAEA-AEBmAIUoALgLsICChAjGIAEGCcYigXCAgoQABiABBhDGIoFwgIFEC4YgATCAggQABiABBjLAZgDAJIHCjItMTUuMy4xLjGgB51-&sclient=gws-wiz-serp
 https://ru.legacy.reactjs.org/docs/conditional-rendering.html
 https://www.youtube.com/watch?v=J3aFEqouK44&t=252s
@@ -57,17 +66,26 @@ https://www.youtube.com/watch?v=2_4WBH-Si0w&list=PLXZzYzG2cy7yd7rDkqRSmJO0QGjdEe
 https://www.youtube.com/watch?v=PYWaUwjofJo&list=PLXZzYzG2cy7yd7rDkqRSmJO0QGjdEeBqK&index=54
 https://www.youtube.com/@cameronmcnz/videos
 https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories
+https://www.electronjs.org/
+https://reactnative.dev/
+https://docs.docker.com/guides/nodejs/containerize/
+https://www.youtube.com/watch?v=prOarIqL5Qs
+https://jekyllrb.com/
+https://docs.strapi.io/dev-docs/api/rest/filters-locale-publication
 
 ---
-git command & Conflicts:
+git command/npm & Conflicts:
 https://stackoverflow.com/questions/10116373/git-push-error-repository-not-found
 https://stackoverflow.com/questions/39837559/why-isnt-my-page-loading-up-on-github
 https://stackoverflow.com/questions/20101994/how-to-git-pull-from-master-into-the-development-branch
 https://opensource.com/sites/default/files/u128651/git_guide4.png
 https://stackoverflow.com/questions/4181861/message-src-refspec-master-does-not-match-any-when-pushing-commits-in-git
+https://stackoverflow.com/questions/78655688/cannot-find-module-ajv-dist-compile-codegen
+https://stackoverflow.com/questions/70182608/i-am-trying-to-run-npm-start-in-my-terminal-but-it-isnt-working
 
 ---
 Тренажёры и Ответы:
+https://solvit.space/?utm_source=yt_channel_artemshumeiko&utm_medium=organic&utm_campaign=desc_box&clckid=6a608c4e
 https://leetcode.com/problems/binary-search/description/
 https://codepen.io/trending 
 https://www.codewars.com/collections/javascript-basics-2
@@ -108,9 +126,23 @@ https://getmatch.ru/vacancies/20299?s=applications_status
 Собеседования:
 Запись собесов https://www.google.com/search?q=pfgbcm+cj%2Cctljdfybz+rfr+ltkfnm+xthtp+ntktuhfvv&oq=pfgbcm+cj%2Cctljdfybz+rfr+ltkfnm+xthtp+ntktuhfvv&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQIRgKGKABMgkIAhAhGAoYoAEyCQgDECEYChigAdIBCjE0MTEwajBqMTWoAgCwAgA&sourceid=chrome&ie=UTF-8
 
---- 
-Сервера:
+---
+VPS сервера:
+https://www.scaleway.com/en/pricing/elastic-metal/
+https://simplecloud.ru/price/
+https://poiskvps.ru/vps/nedorogoy
+https://habr.com/ru/articles/332130/
+https://cp.beget.com/cloud/servers/mern-cloud-13
+https://my.selectel.ru/login-sessions
 https://my.selectel.ru/balance/add
+https://my.selectel.ru/registration
+https://habr.com/ru/articles/672526/
+https://www.smartape.ru/neogranichennyj-hosting
+https://dashboard.render.com/login?next=%2Fweb%2Fnew%3FnewUser%3Dtrue
+https://signup.cloud.oracle.com/?intcmp=OcomFreeTier_cis&language=ru_US
+https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-20-04
+https://www.youtube.com/watch?v=vPVAYsc-U60&list=PLTOFW1FqT6SwOevVmg2dyxDGu-tT5d49K
+https://ru.hostadvice.com/cloud-hosting/free/
 
 --- 
 Беспл. Курсы:
@@ -149,16 +181,6 @@ https://habr.com/ru/companies/getmatch/articles/650337/
 https://habr.com/ru/companies/getmatch/articles/547028/
 
 ---
-VPS сервера:
-https://www.scaleway.com/en/pricing/elastic-metal/
-https://simplecloud.ru/price/
-https://poiskvps.ru/vps/nedorogoy
-https://habr.com/ru/articles/332130/
-https://cp.beget.com/cloud/servers/mern-cloud-13
-https://my.selectel.ru/login-sessions
-
-
----
 Нетворкинг и тусовки:
 https://www.google.com/search?q=DRINKUP+IT+%D0%9C%D0%9E%D0%A1%D0%9A%D0%92%D0%90&oq=DRINKUP+IT+%D0%9C%D0%9E%D0%A1%D0%9A%D0%92%D0%90&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQABgNGIAEMgcIAhAAGO8FMgcIAxAAGO8FMgcIBBAAGO8F0gEKMTA5NDRqMGoxNagCCLACAQ&sourceid=chrome&ie=UTF-8
 https://www.google.com/search?q=it+events+moscow&oq=IT+events+&gs_lcrp=EgZjaHJvbWUqBwgBEAAYgAQyBggAEEUYOTIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIHCAUQABiABDIHCAYQABiABDIHCAcQABiABDIHCAgQABiABDIHCAkQABiABNIBCjEyNzQ0ajBqMTWoAgiwAgE&sourceid=chrome&ie=UTF-8
@@ -168,4 +190,5 @@ https://digitalhr.today/recruitingtech?lang=ru
 ---
 Для работы в проектах:
 https://strapi.io/
+https://docs.strapi.io/dev-docs/api/rest/filters-locale-publication
 
