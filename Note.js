a7dbbf57412135bc90858fa64716e76eea48d0e2
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-04 17:12:11 +0300

    Create Note.js

---
+++ Note.js
@@ -0,0 +1,14 @@
+[Основы алгоритмов]:
+---
+Алгоритмы из яндекса:
+https://www.youtube.com/watch?v=8pRGuvkzK7Y
+https://education.yandex.ru/handbook/algorithms +
+https://www.youtube.com/results?search_query=%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81+%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC%D1%8B+
+https://www.youtube.com/playlist?list=PL6Wui14DvQPySdPv5NUqV3i8sDbHkCKC5
+---------------------------------------------------------------------------------------------
+📌 [Простые алгоритмы]:
+(в блокноте notepad)
+---
+# тесты и проверка на дебагер в голове подстрочно (happy path сценарий + edge cases)
+# писать просто и понятно - чистый код
+---------------------
