f1e1233dbadd808bad12792d8f91a72e6fb46703
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-30 23:49:55 +0300

    Update Note.js

--- Note.js
+++ Note.js
@@ -126,6 +126,7 @@ https://www.youtube.com/watch?v=4Z2cUITcT3I
 https://www.youtube.com/watch?v=4iME6tUYJEM
 ---------------------
 java coding: 
+https://youtu.be/9vLAPwLeUkY?si=9LsaNaLn2fTvmsHw
 https://youtu.be/b3VxeTvSh08?si=sSPOeJCOKY7I3A_p
 https://youtu.be/i0SM3DSkn0k?si=PweWUKgksQJh49fg
 https://www.youtube.com/watch?v=6Ngul82oI18
