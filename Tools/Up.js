9c32b35a7172292d584042668f9b8ea67fd66a2c
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-09-05 14:25:29 +0300

    Create Up.js

---
+++ Tools/Up.js
@@ -0,0 +1,11 @@
+Вёрстка
+
+https://vc.ru/marketing/2185707-mobile-first-ot-google-kak-podgotovit-sajt-k-2025-godu
+https://htmlacademy.ru/blog/css/pixel-perfect
+https://www.unisender.com/ru/glossary/chto-takoe-pixel-perfect-verstka/#anchor-1
+
+мнемотехника
+https://habr.com/ru/companies/first/articles/861506/
+
+Backend проекты
+https://eax.me/programming-language-learning/
