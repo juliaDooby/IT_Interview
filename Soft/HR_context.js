0a05088f8ee1412560bc42af8fe4f1e2f8c77272
Julia: @juliaDooby <juliadooby@yandex.ru>
2026-06-16 21:51:14 +0300

    Update HR_context.js

--- Soft/HR_context.js
+++ Soft/HR_context.js
@@ -58,3 +58,170 @@
 ------------------------------------
 [Тестовые HR ]:
 Вот недавно сделала тестовое от одной компании, оно очень похоже на то, что вы хотите мне предложить, зайдите на github и проверьте этот вариант!, возможно, он закрывает ваше требование! (ссылка github)
+
+Personal & Work History-Related HR Questions
+Q #1) Tell me something about yourself.
+
+Self Introduction
+Answer: This is the first question that every HR asks in an interview. Usually, this is not only their way of kick-starting the session but also of assessing the poise, communication ability, and delivery style of each candidate.
+
+Don’t get into a mini-speech about your childhood, hobbies, studies, likes, dislikes, etc. It tells them you are not a strong fit for the job. Meandering answers like that give them a legitimate concern that you might have a hard time compartmentalizing responses.
+
+Understand that your recruiter wants to know the real you and keeps the conversation relevant as well as on point. So, it is okay if you digress for 30 seconds, but make sure your side story doesn’t go on for any longer than that.
+
+Talk about your current job and employer, tell them about a few significant accomplishments of yours, and talk about some of your key strengths that they can relate to the current job. Finally, tell them how you think you can fit the job.
+
+Q #2) Why are you looking for a new job?
+
+Answer: If you are or have been working somewhere, you will be asked this question. If you have left your previous job, HR could ask you why. In the answer, they will look for transparency and honesty. If you are one among those who lost their job during the layoff, don’t stigmatize anyone for that.
+
+They will look for the situational context in your answers and will judge your decisiveness, decision-making abilities, and ability to work with others. If you are currently employed, HR will seek solid ground and sound explanations for why you are looking for a new job.
+
+If you are transitioning into a new industry, they will want to know why. They will try to figure out if your answer is credible and fits into the short and long-term responsibilities of the job they are interviewing you for. Try to refocus the discussion on how your skills match the current position to ace this question.
+
+Say something like you enjoy working in your current company. Its culture and people make it a great workplace. However, you are looking for new & fresh challenges and more responsibilities. Tell them you have worked on several projects and have completed many, but the opportunities are currently scarce at your current job.
+
+Q #3) What makes you interested in this job?
+
+jobs_hiring
+Answer: The answer to this question will let them know if you are seriously interested in the role and the company. Or that you are simply applying for any available job. Don’t answer casually or generalize your interest in the job.
+
+Always mention the specific qualifications of the job and explain how they align with your strengths and skills. Demonstrate your passion for the job and a deep interest in the company. Give them data and brief them on why you think this is the job for you and why you are best suited for this job.
+
+Strength & Weakness Related Questions in HR Interview
+Q #4) Tell us about your biggest Strengths.
+
+Answer: This is a stalwart question of interviewing. HR reads a lot in your answers without you realizing it. They will look for an answer that summarizes your work experience, achievements, and strongest qualities that are directly related to the job.
+
+Cite skills like initiative, ability to work in a team, self-motivation, etc. In their experience, those who focus on perceived strengths might not be a good fit for the job. Don’t show over-eagerness to handle the assignments or any such thing that doesn’t come under the described job.
+
+Q #5) Tell us about your Weaknesses.
+
+weakness
+Answer: Everyone has weaknesses, so never say you don’t have any. Also, stay away from the cliche answers like you are a perfectionist and expect the same from everyone, etc.
+
+Say something like your team thinks you are very demanding sometimes and drive them very hard. But now, you are getting good at motivating them instead of pushing them. Or, profess your lack of experience and know-how in a field that is not related and vital to the job.
+
+Q #6) Describe an instance from your life where you messed up.
+
+Answer: This is a tricky question that HR asks intentionally to see if you can learn from your mistakes. If you can’t think of any incident, it could mean you aren’t capable of owning up to your mistakes. Also, too many of them can make you look unfit for the job.
+
+Keep your answers brief and clear. Pick an error that doesn’t show a lack of character. Describe a well-intended error and finish with how that experience helped you grow.
+
+For Example, say that in your first job as a manager, you took on too many tasks that made you become less efficient and feel overwhelmed.
+
+Also, your team members felt a lack of collaboration that frustrated them. You quickly realized that you will have to learn how to delegate tasks and collaborate with your team. That turned you into a successful manager, etc.
+
+Q #7) Have you ever experienced conflict with your co-worker? How did you deal with it?
+
+Conflict with co-worker
+Answer: This question is to know how you handle workplace conflicts. The interviewer isn’t interested in knowing the story of the time when your co-worker said some snide things about you or when your manager overheard you gossiping about a client.
+
+Conflicts are inevitable at offices. You work with different people, and you are bound to feel the friction with some of them. HR wants to know if you can resolve the conflict without pointing fingers. The main focus of your answer must be the solution, and your efforts must show a level of empathy towards your colleagues.
+
+Say something like you had to meet a deadline and you needed some input from one of your colleagues to finish the project. But as the deadline approached, your colleague wasn’t ready with the input delayed your project, and made you both look poorly in the eyes of your clients or seniors.
+
+To understand what went wrong, you confronted your colleague in private. You found the solution to the problem and asked for a promise to be transparent in the future so that both of you don’t have to face the same situation again.
+
+Desire & Dislike-Related Recruiter Interview Questions
+Q #8) What do you know about this Industry and our Company?
+
+Answer: This is a wonderful opportunity to impress the HR interviewer. It aims to determine how much you are interested in this company and the industry. So, before you appear for the interview, research well, not just about the company but also the industry.
+
+Your lack of research into the business line of the company, its culture, and other such things can have you eliminated faster than you can imagine. The more you research, the more you can demonstrate your genuine inclination to work with them.
+
+Start with a brief description of the industry and carry on to where the company stands among the companies of that industry. Talk about their product, services, and mission statements. Move on to their work culture and environment and finish with what extracurriculars they emphasize,e along with what about them has caught your fancy.
+
+Q #9) Tell us one thing you like and dislike about your previous/current positions.
+
+like-dislike
+Answer: Go for the answers that are relevant and specific to the position you have applied for. Never say things like it was an easy commute or there were great benefits. It might send you job hunting again.
+
+Instead, be someone who values the same workplace qualities that the company you are interviewing for. Or be the one who can make teams with strong camaraderie. HR would prefer candidates with the above likes and with those who want opportunities on the cutting edge of technology.
+
+When you are talking about the things you don’t like about your present or previous job, you can mention the responsibility areas that are not connected in any way to the job you are applying for. If you have performed any undesirable task or have learned something from a bitter experience, mention that.
+
+It will showcase that you can even do the tasks that don’t interest you, and you will prove to be a gem.
+
+Q #10) How do you stay motivated?
+
+Answer: Benefits and money motivate everyone, but don’t say this as your answer. Instead, tell them you are extremely result-oriented and that getting the job done in the way you wanted motivates you a lot. Tell them that things like working on your project, the buzz of working in a team, taking on challenges, etc., motivate you a lot.
+
+Mention things like working towards a goal, developing your skills, the quest for personal development, job satisfaction, contributing to a team effort, excitement for new challenges, etc., but never mention materialistic things.
+
+Other Important HR Interview Questions with Detailed Answers
+Q #11) Why should we hire you?
+
+Reason for hiring
+Answer: In the answer to this question, talk about your accomplishments and your strengths. Tell them you keep motivating your team members with your excellent methodologies. Incite references to the incidents where you have successfully encountered challenges and have met deadlines.
+
+If you haven’t worked previously, connect your studies to the requirements of this job. If you have interned in any company, let them know how that period has helped you in developing the skills relevant to this job.
+
+Say something like you have the perfect combination of experience and skills required for this job. Tell them you have strong problem-solving and analytical skills that you have earned with your work experience. You are dedicated to delivering excellent results and adding value to the company.
+
+Remember to emphasize your unique skills concisely and highlight your strengths, accomplishments, and skills. With an example, demonstrate yourself as a quick learner and that you have contributed to the growth of your previous company.
+
+Never say I need the job or money, or that you want to work somewhere closer to home. Never compare your skills to those of others.
+
+Q #12) How will you add value to our current Products and Services?
+
+Answer: With this question, HR wants to know if you are innovative and can think quickly. It will tell them if you can bring new ideas to the job. Show some creativity in your answers and plan. Think about the potential problems that the company might be experiencing with their services and products, and how you can fill that void with your unique skill set.
+
+For Example, you can say that you have noticed that their products and services are all in English and that too without the option of translation. Tell them how multilingual translations can benefit their appeal to a wider demographic and become more of a global leader.
+
+Q #13) Don’t you think you are underqualified/overqualified for this job?
+
+overqualified
+Answer: If you are underqualified, focus on the skill sets and experiences that you will be bringing to the position. Keep away from the lengthy explanations that can offer real insights into your true motivations, bad or good, for looking for the job.
+
+It is not uncommon for anyone to seek a position that has fewer responsibilities where you can be a mentor to the younger employees and be a strong team player. So, it is evident that they will count you as overqualified, but don’t let them reject you on that basis. Tell them how your experience can benefit the company.
+
+Q #14) Do you prefer working alone or with others?
+
+Answer: The basic intention of HR behind this question is to know if you can work with a team. If you say, team, they might assume that you can’t work in a team, and if you say, alone, they might assume that you are not a team player.
+
+You must frame your answer in a way that makes them believe that you can work in a team and still handle individual responsibilities. Beforehand, make sure if the job requires a team player or an alone worker, or both.
+
+You can say something like you like working with a team, as you think you can get more work done when everyone is taking part. However, you also enjoy working alone when needed, as you don’t need to be constantly reassured of your work.
+
+Q #15) How compatible are you with different types of people?
+
+compatible with others
+Answer: Offices are loaded with various people of various personalities. With this question, the interviewers want to know if you will get along with them. Your answer must tell them that the kind of people you work with doesn’t matter to you. You just focus on getting the work done.
+
+Never badmouth your supervisors or colleagues. They will be keeping their ears open for negative answers. Don’t give it to them. Turn the negativity into positive answers.
+
+Q #16) Are you a go-getter?
+
+Answer: To answer this question, share an incident where you have put long hours into a project to meet a deadline. In the end, you completed the task or the project on time, and that too under the budget, which made you and your company look good.
+
+Cite incidents where your boss appreciated you and you became one of the most reliable employees. Tell them you are dependable and can get things done without supervision, and your boss, colleagues, and clients appreciate you for that.
+
+Q #17) What led you to this particular profession?
+
+Answer: When you are answering this question, you need to be precise. Tell the HR what inspired you to take this particular profession or career path. But make sure you keep your answers short and to the point.
+
+Don’t say you picked a job or majored in a subject because you thought it would be easy. Tell them you picked this career path because you were fascinated, or inspired by the field, or what you can achieve through it.
+
+Q #18) Tell us about something that bothers you.
+
+Answer: Through this question, the interviewer is trying to know what bothers you related to the people you work with or the job. If other people or their ideas bother you, don’t say that in your answer. Tell them something like when people don’t deliver their promise or meet their deadline, it bothers you.
+
+Q #19) Are you willing to relocate?
+
+Relocate
+[via Axios]
+
+Answer: This is a straightforward question and needs a straightforward answer. Companies often look for candidates who can easily accept transfers and are comfortable moving around. If you are okay with that, your chances of being selected are high. But be honest. If you are not comfortable with the idea of relocation, say no.
+
+It might become a reason for conflict later if you say yes now and deny it later. It might even tarnish your reputation somewhat. So, if you can’t relocate, just say no. If you are a promising candidate, they will not let you go for such a trivial matter, unless relocating is a major part of the job profile.
+
+So, frankly, put your answers in front of HR and hope for the best.
+
+Q #20) Do you have any questions for us?
+
+Answer: Never say no to this question. Often candidates say no in their excitement, and that’s a mistake. But remember one thing, always have questions for HR. Having some strategic, thoughtful, and smart questions will demonstrate your genuine interest in the job and the value you can potentially add to the profile and the company.
+
+Remember, HR is looking for candidates who will ask questions and take the company forward. That can’t happen if you accept everything the same way they are. In the answer to this question, you must voice your genuine concerns regarding this role. You can ask HR what they enjoy most about working there, or what is one thing you need to keep in mind while working here, etc.
+
+Ask a few questions that show your interest and dedication to the company and the job. You can also ask questions like What is the most challenging aspect of this job profile. You can also ask what the scope of professional development is in the department and what the role is.
