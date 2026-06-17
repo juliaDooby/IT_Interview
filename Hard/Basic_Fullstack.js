1dda60109cc1de0afc94f1c1599bf958185ca2b8
Julia: @juliaDooby <juliadooby@yandex.ru>
2026-06-17 16:26:32 +0300

    Update Basic_Fullstack.js

--- Hard/Basic_Fullstack.js
+++ Hard/Basic_Fullstack.js
@@ -117,3 +117,242 @@ SQL databases are relational, using structured schema and fixed data models. The
 Addressing this question requires a two-fold understanding: frontend error handling for user experience and backend error handling for system stability.
 
 On the frontend, I ensure errors are gracefully handled by giving the user meaningful feedback, and I employ retry mechanisms for transient issues. Backend errors are logged with detailed context for diagnosis. I use middleware for centralized error handling in frameworks like Express.js. Monitoring tools like Sentry help in real-time error tracking, while proper status codes in API responses aid the frontend in deciding the next steps.
+
+50 Interview Questions for Full-Stack Developers
+What is your understanding of multithreading?
+It is a technique that allows a system or program to perform multiple tasks at the same time within a single process. Multithreading is approached by splitting the functionalities or program work into smaller, independent units, which are more manageable, called threads.
+
+Client-side and server-side programming are different things. How?
+The client-side and server-side rendering are two different aspects of a web application that work together that deliver top-notch functionality to the users.
+
+The client side is the part of the application that runs in the user’s browser. In simple words, it is the side of the application that the user views and interacts with. 
+
+Whereas the server side of the application is the part that operates on the server and processes requests from the client, performing tasks like database queries, business logic, etc.
+
+State some of the markup languages available.
+There can be a long list of markup languages, but here are the most commonly used markup languages:
+
+HTML
+KML
+MathML
+XHTML
+XML
+What is !DOCTYPE?
+It is a type of instruction that informs the browser about the markup language in which the page is written. It is neither an element nor a tag, but it lets the browser know about the version of HTML or any other markup language being used.
+
+What are the roles of HTML, CSS, and JavaScript in web development?
+The role of HTML in web development is to define the structure and content of a webpage. On the other hand, CSS handles the styling of the webpage, including colors, layout, fonts, etc. Lastly, JavaScript adds interactivity and dynamic behavior to the webpage.
+
+What are data attributes?
+Data attributes are the data fields that define the properties of a data object. They are mostly found in a table format, but can also refer to special values for objects in programming languages.
+
+What is your understanding of static and dynamic websites?
+The content delivery and updation are different in static and dynamic websites. Static websites are built using HTML, CSS, and JavaScript. Their content doesn’t change each time you access it. On the other hand, dynamic websites are built using server-side programming languages and often databases to store and retrieve information.
+
+What practices do you follow to ensure code quality and maintainability?
+There are many practices that one should observe to maintain code quality and ensure maintainability. Some of the practices include:
+
+Use consistent coding standards.
+Follow consistent style guides.
+Utilize tools like Prettier or ESLint for enforcing strict linting and code formatting.
+Implement unit testing and integration testing.
+Opt for CI/CD pipelines to maintain quality throughout the development process.
+Explain REST API.
+Representational State Transfer Application Programming Interface is a standard way for web applications to communicate over HTTP by following a set of principles. It allows clients, such as web browsers or mobile applications, to interact with servers to perform various operations.
+
+Explain the GET and POST HTTP methods.
+GET is used to retrieve data from the server and is read-only. Whereas POST sends data to the server to create or update resources.
+
+State the core differences between relational and non-relational databases.
+The relational database stores data in structured tables with rows and columns. It is widely used and good for relationships and complex queries. On the other hand, non-relational databases store data in flexible formats like documents, key-value pairs, or graphs. It is good for unstructured or hierarchical data.
+
+How do you take a responsive design?
+A website design is said to be responsive if the website looks good on all devices. This means that it adapts to any screen layout, design, and size.
+
+What is the difference between the <div> and <span> tags?
+The <div> tag is a block-level element best used to attach to a web page section. It is made to accept the align attribute. Moreover, this tag should be used to wrap a section or highlight that section.
+
+The <span> tag is an inline element that is best used to attach CSS to a small section of a line on a web page. It is not capable of accepting aligned attributes. Further, this tag wraps any specific word that you want to highlight on your webpage.
+
+Briefly tell the steps to optimize the performance of a React app.
+Reduce re-renders by using React.memo and useCallback.
+Implement lazy loading using React.lazy and Suspense.
+For efficient state management, keep the state local where possible.
+Minimize the DOM updates.
+Implement code splitting.
+Use React Developer Tools to identify issues.
+Describe pair programming.
+Pair programming is a programming technique that involves two programmers working together on a single machine. In this technique, one programmer has the role of writing code while the other plays the role of an observer. He observes the code and ensures the code is written correctly, proofread, and logic-checked.
+
+Moreover, the roles can be swapped at any time.
+
+How will you decrease the load time of your web application?
+Following the given steps, you can achieve better load times for your web application.
+
+Optimize the image size of the images present on your web pages.
+Keep JavaScript and CSS in external files.
+Try to minimize the redirects.
+Load CSS and JavaScript files asynchronously.
+Reduce the sizes of HTML, CSS, and JavaScript files.
+What is your understanding of dependency injection?
+It is a design pattern that implements the Inversion of Control principle. It enables the creation of dependent objects outside of classes and makes them available to classes in various ways.
+
+Here are the types of classes involved in Dependency Injection:
+
+Client Class
+Service Class
+Injector Class
+State some latest full-stack development trends.
+According to the modern trends of full-stack development, we are witnessing the following things in 2025.
+
+Low-code and no-code development.
+Blockchain technology.
+Augmented reality and virtual reality.
+Progressive web application development.
+Artificial intelligence and machine learning.
+MVC is a very popular architecture. What do you know about it?
+MVC, Model-View-Controller, is a modern design pattern that is widely used as a software architecture for web application development. Following this approach, you get to break down the complex software development into three components.
+
+Model: It represents the data and business logic of the application. Its job is to manage data, perform calculations, and address the requests from the controller.
+View: It is the user interface of the application, which displays data from the model to users and provides a platform to interact with the application.
+Controller: The Controller is responsible for handling the model and view for managing the flow of the application. It includes data flow within the model object and updates the View whenever there is a change in data.
+Which languages are preferable for full-stack development?
+For full-stack development, developers prefer C++, C#, Python, Java, PHP, Ruby, etc for backend programming. While developers opt for React, Angular, Next JS, etc.
+
+In your opinion, what are the benefits of using Python?
+The core perks of Python are easy syntax and smooth readability. However, it furnishes a list of benefits:
+
+It is easy to learn and write compared to other languages.
+Python comes with extensive support libraries.
+It is an open-source language.
+There’s no need to mention the data type in Python, as it automatically takes the data type.
+It is an interpreted language with line-by-line code execution. This promotes easy debugging and portability.
+It is suitable for data analysis.
+What is CORS?
+It is the short form of Cross-Origin Resource Sharing. It is a security feature that restricts web pages from making requests to a different domain than the one they were loaded. CORS is used by browsers to prevent malicious cross-site requests.
+
+What are the perks of pair programming?
+If the programmer gets stuck on any problem, the second programmer acting as an observer will take the driving seat to fix the issue.
+The code quality increases, and the chance of error reduces.
+Pair programming enhances communication, which leads to a more productive and positive work environment.
+Two developers working together give collective code ownership, promoting a shared understanding and responsibility for the entire project.
+Can we add comments in CSS?
+Yes, you can add comments in CSS. Adding comments in the CSS helps you explain the code and make the program readable and understandable.
+
+All you have to do is add “/*” at the start of the comment and “*/” at the end of the comment.
+
+Syntax for comments in CSS
+
+/* content */
+
+Using this, you can add single and multi-line comments.
+
+How do you add a comment in JavaScript?
+For a single-line comment, you add a double slash “//” before the comment. Whereas to add multiline comments, you have to start with “/*” and end with “*/”.
+
+Explain your understanding of the observer pattern.
+It is a behavioral design pattern used to define a one-to-many dependency between objects, allowing loose coupling between them.
+
+It enhances scalability while allowing for easy addition or removal of observers without affecting the subject. Moreover, it facilitates event-driven programming.
+
+What’s the importance of version control systems?
+Version Control Systems help you manage changes in a project codebase, enabling collaboration, tracking modifications, and facilitating rollback to previous versions. Moreover, it assists in error identification and debugging.
+
+Briefly explain the concept of negative infinity.
+It is a constant value that represents the lowest available value and is the opposite of positive infinity. It is represented by “-∞”.
+
+Briefly explain the process of deploying a full-stack application to a cloud provider.
+There are different stages of deploying full-stack applications to a cloud provider. To deploy an application, you have to follow the steps below:
+
+Prepare the application by building the front end and ensuring the back end is production-ready.
+Deploy the front end by pushing the code into the servers, such as AWS S3, Google Cloud Platform, or Firebase hosting, to host static files.
+Deploy the back-end using cloud services like AWS EC2, Google Cloud Platform Compute Engine, etc. Set up environment variables and connect to the database.
+Use a database for your server.
+Configure a custom domain and HTTPS.
+What is NULL in JavaScript?
+It is just an empty value or object. The NULL basically represents no value or no object.
+
+Elaborate something about a prompt box.
+It is a dialog box that displays an optional message that prompts the user to input some text. The purpose of the prompt box is to gather user input or confirm actions before proceeding.
+
+What do you know about the Virtual DOM of React?
+The Virtual DOM in React is a representation of the actual DOM. The job of Virtual DOM is to efficiently update and render the user interface by comparing the current and previous virtual DOM without affecting the actual DOM.
+
+Describe something about JSX.
+It is a syntax extension for JavaScript. It allows developers to write HTML-like code within the JavaScript files. JSX is commonly used in React for building user interfaces as it makes things easier for developers by allowing them to write HTML-like code directly in JavaScript.
+
+Explain a little about MongoDB.
+MongoDB is a NoSQL database. It is different from the traditional relational database structure that ensures flexibility and furnishes a document-oriented data model. MongoDB stores data as JSON-like documents, which makes it easier to handle unstructured data and provides greater flexibility for schema design.
+
+When should we use Express.js?
+You may opt for Express.js when you want your application to remain simple, minimalistic, flexible, and scalable. Moreover, it gives an easy setup for middleware and routing. Its lightweight structure makes it a top-notch choice for building server-side web applications that are not only smart but also faster.
+
+What are the ways to apply CSS to your HTML file?
+There are three ways to apply CSS in your HTML file. Inline CSS, internal or embedded CSS, or external CSS. All three ways have their perks and disadvantages, and developers choose the one according to their needs and ease.
+
+How will you detect the operating system on the client machine?
+To detect the operating system on the client’s machine, you have to simply use navigator.appVersion or navigator.userAgent property. This will return you the information about the version of the browser or operating system.
+
+What is the role of Hooks in React?
+Ract Hooks are React functions that allow functional components to access state and other React features. They help you in state management, perform side effects such as data fetching, DOM manipulation, etc, simplified logic, and code reusability.
+
+Describe your knowledge of API.
+API is the short form of Application Programming Interface. It is a set of rules that allow various software applications to interact with each other. Moreover, its job is to define how requests and responses should be handled, enabling smooth data exchange between systems.
+
+Briefly tell about cookies and session storage.
+Cookies are small pieces of data stored on the user’s browser. They play their part in tracking, authentication, and managing user preferences. Cookies are sent with every HTTP request, which makes them useful for session management.
+
+Talking about session storage, it stores the data of a single session. This means that it only works until a tab or browser window is open. Once the window closes, the data vanishes.
+
+What is CMS?
+A CMS is a software platform, known as a content management system. It allows users to create, edit, and manage website content without any coding knowledge. It is an easy and user-friendly way of handling text, images, and other media content on your website. Some common CMS platforms include WordPress, Joomla, and Drupal.
+
+Progressive Web Applications are a modern way of developing websites. What do you know about it?
+Yes, many businesses prefer progressive web applications for their operations. A PWA is a type of web application that comes with the features of a traditional website and a native mobile app. The best thing about PWAs is that they work offline.
+
+What’s the core difference between normalization and denormalization?
+Normalization is the process of removing redundant data from a database and storing consistent, non-redundant data. On the other hand, denormalization involves the combination of data from various tables into a single table to carry out queries quickly.
+
+Moreover, normalization focuses on clearing out unused data, reducing duplicate data, and eliminating inconsistencies from a database. Whereas, the goal of denormalization is to achieve faster query execution by adding data redundancy.
+
+Describe event bubbling and event capturing.
+The propagation of events in JavaScript is known as Event Flow. It has two aspects:
+
+Event Bubbling
+It is the event that is captured and handled first by the innermost element. It then propagates to the outermost element. 
+
+Event Capturing
+In event capturing, when an event is captured, it is handled by the outermost element and later propagated to the innermost element.
+
+Tell me about the different states of JavaScript promise.
+There are three states of JavaScript promise:
+
+Pending: It is the initial state, which is neither completed nor rejected.
+Fulfilled: It is the completed state, which means the operation is successful.
+Rejected: Operation has failed.
+What is a Request Dispatcher?
+It is an interface in Java Servlets that is used to forward a client request to a resource. Request Dispatcher assists in dividing a web application into smaller modular components, which are easy to manage and maintain.
+
+Mention some instances where you would choose Python as your primary programming language.
+Data analysis
+Numerical computing
+Machine learning and artificial intelligence
+Automation
+Game development
+Natural language processing
+Computational intelligence
+Big data analytics
+What strategy do you follow to remove array duplicates in ES6?
+For removing array duplicates in ES6, using a set is the best strategy. The reason is, that it only allows you to store unique values, and when you convert an array to a set, it eliminates duplicate values.
+
+Then you need to convert the set back to the array with unique values.
+
+Describe your methodology for handling errors in the GraphQL server.
+For error handling in a GraphQL server, it is best to use a custom error format, adding error codes, and wrapping resolver functions with error-catching middleware or higher-order functions. Moreover, to efficiently deal with errors, you should properly categorize and return to the client in a structured format.
+
+In your opinion, what are the challenges of building full-stack applications?
+In my opinion, the following are the challenges on the road to building full-stack applications.
+
+Managing state across front-end and back-end. For this, it is better to use global state management tools.
+There are scalability issues in building full-stack applications that can be handled by optimizing database queries, implementing caching, and using scalable cloud infrastructure.
+Security concerns are another challenge in full-stack applications, which can be eliminated by bringing in secure authentication, sanitizing inputs, and following best practices.
