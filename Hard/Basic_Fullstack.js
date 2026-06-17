d38ae4f744336cb71f92c6543e71caedaddfbf36
Julia: @juliaDooby <juliadooby@yandex.ru>
2026-06-17 19:02:19 +0300

    Update Basic_Fullstack.js

--- Hard/Basic_Fullstack.js
+++ Hard/Basic_Fullstack.js
@@ -4656,3 +4656,26 @@ const int a
 Наконец, меня предложили вместе с еще шестью студентами.
 
 Вниманию читателя! Не прекращайте учиться сейчас. Освойте все важные концепции DSA с помощью самостоятельного курса DSA по приемлемой для студентов цене и будьте готовы к работе в отрасли. Чтобы завершить подготовку от изучения языка к DS Algo и многому другому, см. Полный курс подготовки к собеседованию . Если вы готовы, проверьте свои навыки с помощью серий тестов TCS, Wipro, Amazon и Microsoft.
+
+18 Full-Stack Developer Interview Questions to Use in Your Next Interview
+General Questions
+What would you say was your best implementation experience?
+What are 5+ ways you could optimize a website to be as efficient and scalable as possible?
+What is your checklist for doing code-reviews?
+Walk me through a production issue you troubleshot in the past 12 months.
+What strategies would you pick up for measuring the success of a new feature?
+What considerations would you have for choosing the storage for the application?
+Problem-Solving Questions
+When presented with a new project, how did you select the tools and technologies involved?
+How would you prevent a bot from scraping your publicly accessible API?
+[For lead engineers] If you are tasked with doubling the production scale of a system consisting of multiple components and services, what would your approach be in the span of three months?
+How do you perf tune your applications? What is your approach to improve throughput, latency? (discuss tradeoffs, use cases, examples)
+How would you debug and resolve a site issue?
+How would you find if the deployment is inconsistent?
+Technical Communication Questions
+What is your favorite programming language and why?
+Say you had to write an endpoint for checking if a resource exists. What path and method would you use?
+List three key things to consider when coding with SEO in mind.
+What is the difference between a blue/green deployment and a rolling deployment?
+Explain the difference between REST and GraphQL
+How can you share code between files?
