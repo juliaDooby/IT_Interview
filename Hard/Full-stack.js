b1791722defd8a1dea57d1b143819b0b63612291
Julia: @juliaDooby <juliadooby@yandex.ru>
2026-09-05 12:44:47 +0300

    Update Full-stack.js

--- Hard/Full-stack.js
+++ Hard/Full-stack.js
@@ -137,7 +137,251 @@ SOLID is an acronym representing five design principles: Single Responsibility, 
 
 Secure a Java web application by implementing proper authentication, authorization, input validation, using encryption, and regularly updating dependencies to patch vulnerabilities.
 
-FULL-STACK DEVELOPER INTERVIEW QUESTIONS AND ANSWERS
+
+TCS FULL STACK INTERVIEW QUESTIONS – FREE PDF
+
+Overview of TCS
+Tata Consultancy Services (TCS) is a part of the Tata Group, one of India’s largest and oldest business conglomerates. Founded in 1968, TCS has grown to become one of the world’s leading IT services firms, with a presence in over 46 countries and a workforce of more than 600,000 employees. TCS offers a wide range of services, including consulting, IT infrastructure services, engineering and industrial services, and business process outsourcing.
+
+Tata Consultancy Services (TCS) is an Indian multinational information technology (IT) service, consulting and business solutions company headquartered in Mumbai, Maharashtra. It is a subsidiary of the Tata Group and operates in 47 countries. As of 2026, Tata Consultancy Service is ranked 14th overall in the Forbes World’s Most Innovative Companies ranking, making it both the highest-ranked IT services company and the top Indian company on the list.
+
+Experience the power of our web development course with a free demo – Enroll now!
+
+Why Join TCS as a Full Stack Developer?
+1: Which of the following is a JavaScript framework/library?
+
+Next
+Ever wondered how much you really know? It's time to put your brain to the test!
+Attend Free Quiz
+1. Work-life balance
+Often, people working in software industry find it very hard to maintain the work-life balance. However, 23% people believe that TCS offers them opportunity to balance their work as well as their personal life.
+
+2. Brand name
+In recent years, IT companies have grown like mushrooms, especially in India but only a few are able to provide a sustainable career to their employees. That’s why people prefer working with big brands. Getting associated with a brand not only gives you a satisfaction that you are going on a right career path, but also ensure job security to some extent. About 23% employees (from the 4,600+ responders) think that TCS is a brand that can help them boost their career.
+
+3. Work environment
+Another important factor that lures the workforce towards TCS is the company’s work environment. About 16% of employees voted TCS for providing a favourable and healthy work environment.
+
+4. People/colleagues
+Colleagues are the people with whom you spend most of your time at workplace. Also, for running a project successfully, an understanding and cooperative team is an absolute necessity. About 15% of the respondents say that it is one of the major reasons they are associated with TCS.
+
+5. Leave policy
+People tend to like a company that understands the needs of its workforce. An employee-friendly leave policy and regulations are among the top five reasons why people continue to work with the firm. 14% people have approved of TCS of having good leave policy.
+
+🚀 Start Coding Today! Enroll Now with Easy EMI Options. 💳✨
+Get Hired as a Full-Stack Developer: Master the Skills Employers Are Looking For!
+
+Start Learning With EMI Payment Options
+TCS Full Stack Developer Interview Questions 2026
+The interview process at TCS for a full stack developer position is comprehensive, covering a range of topics from technical skills to problem-solving abilities and cultural fit. Here is a detailed list of potential interview questions you might encounter:
+
+I) Technical Questions:
+1. What do you know about Full stack Web Development?
+Answer: Full stack web Development refers to the development of both front end and back end of web application. They work on the frontend, backend, database, and debugging of web applications.
+
+2. Which Tools are used for Full stack web development?
+Answer: Some of the important Full stack development tools are:
+
+Code editors like Visual Studio Code, IntelliJ IDEA, and Sublime Text.
+Browser Developer Tools
+Frameworks like Angular, React, and Vue.js
+CSS Preprocessors like Sass and LESS
+Responsive Design Tools like Bootstrap.
+3. What is the difference between Front end and Back end development?
+Answer: Front end is the visible part of web application or website, in which the user interacts directly. Whereas, back end is server-side development of web application or website. It contain all the code required for the application to run, including integrations with data systems, communicating with other applications, and processing data.
+
+4. Which languages are required for front end web development?
+Answer: HTML, CSS, JavaScript are the most common front end languages.
+
+5. Can you write the basic structure of HTML.
+Answer: Basic structure of HTML is as follows:
+
+<html>
+
+<head>
+<title>Page Title</title>
+</head>
+
+<body>
+<h2>Heading</h2>
+<p>Hello World</p>
+</body>
+
+</html>
+
+6. How to create a table in HTML? Show an example.
+Answer: An HTML table is created with an opening <table> tag and a closing </table>
+<html>
+
+<head>
+<title>Page Title</title>
+</head>
+
+<body>
+
+<h2>Heading</h2>
+<p>Hello World</p>
+
+<table>
+<tr>
+<th>Name</th>
+<th>Age</th>
+</tr>
+
+<tr>
+<td>John</td>
+<td>34</td>
+</tr>
+
+<tr>
+<td>Williams</td>
+<td>42</td>
+</tr>
+
+</table>
+</body>
+
+</html>
+
+ 
+
+Output:
+
+Heading
+Hello World
+
+Name	Age
+John	34
+Williams	42
+7. What is the difference between <th> tag and <td> tag
+Answer: <th> tag is used for table header cells while <td> tag is used for table data cells.
+
+8. What is colspan and rowspan in HTML? What is its syntax?
+Answer: Colspan Allows a single table cell to span the width of more than one cell or column. While Rowspan allows a single table cell to span the height of more than one cell or row.
+
+Syntax:
+
+<tr>
+<th colspan=”2″>Alice</th>
+<th colspan=”2″>Bob</th>
+<th colspan=”2″>Julie</th>
+</tr>
+<tr>
+<th rowspan=”3″>Marks</th>
+<th rowspan=”3″>Percentage</th>
+</tr>
+9. How to add CSS to HTML?
+Answer: To add CSS to HTML, there are 3 primary methods:
+
+Inline CSS: Apply styles directly to HTML elements using the style attribute.
+Internal CSS: Use a <style> tag within the <head> section of the HTML document.
+External CSS: Link to an external CSS file using a <link> tag within the <head> section.
+10. How to add JavaScript externally?
+Answer: To add JavaScript externally to an HTML document, we should create a separate JavaScript file and then link to that file from your HTML document using the <script> tag with the src attribute.
+
+Syntax: <script src = “script.js”></script>
+
+11. What is PHP?
+Answer: · PHP is an acronym for “Hypertext Preprocessor“.  PHP is an open source server side scripting language specifically used for web development.
+
+12. What are the differences between CSS Grid and Flexbox.
+CSS Grid provides two-dimensional layout capabilities.  This means it can handle both rows and columns. Flexbox was designed for layout in one dimension. It handles either a row or a column at a time.
+CSS Grids helps you create the outer layout of the webpage. You can build complex as well responsive design with this. This is why it is called ‘layout first’. Flexbox mostly helps align content & move blocks.
+13. What is virtual DOM and what are its benefits
+The virtual DOM (VDOM) is a programming concept where an ideal, or “virtual”, representation of a UI is kept in memory and synced with the “real” DOM by a library such as ReactDOM.
+
+Benefits of the Virtual DOM
+
+Performance Optimization:
+The virtual DOM reduces the number of direct manipulations of the real DOM, which can be slow and resource-intensive. By batching updates and applying only the necessary changes, React improves overall performance.
+Declarative UI:
+Developers can write declarative UI code, focusing on how the UI should look based on the current state rather than managing the UI updates manually. React handles the efficient updates behind the scenes.
+Simplified Development:
+The virtual DOM allows developers to think in terms of component states and props, abstracting away the complexity of direct DOM manipulation. This makes the development process more intuitive and less error-prone.
+Cross-Platform Consistency:
+The virtual DOM provides a consistent way to describe the UI, regardless of the platform. This makes it easier to create applications that work across different environments, such as web, mobile, and desktop.
+Improved Debugging:
+Since the virtual DOM maintains a representation of the UI in memory, it can be used for debugging purposes. Tools like React DevTools allow developers to inspect the virtual DOM and understand how state changes affect the UI.
+Component-Based Architecture:
+The virtual DOM complements React’s component-based architecture, enabling reusable, self-contained components. Each component manages its own state and virtual DOM, promoting modularity and maintainability.
+14. What are the main differences between Angular, React, and Vue.js?
+Angular, React, and Vue.js are three of the most popular JavaScript frameworks and libraries for building web applications. Here are the differences:
+
+Angular
+
+Angular is a full-fledged framework for building client-side applications.
+It primarily uses TypeScript, a statically typed superset of JavaScript. This offers strong typing and advanced features for large-scale applications.
+It follows a component-based architecture but with a more opinionated structure. It uses modules, components, services, and dependency injection.
+It is suitable for large-scale enterprise applications where a comprehensive solution and strong typing are beneficial.
+React
+
+React is a JavaScript library focused on building user interfaces, particularly single-page applications.
+It primarily uses JavaScript (often with JSX, a syntax extension for embedding HTML within JavaScript). TypeScript is also commonly used.
+It follows a component-based architecture with a focus on a unidirectional data flow. It uses functional or class components.
+It is suitable for applications where flexibility is needed, such as dynamic user interfaces, single-page applications, and mobile apps (using React Native).
+Vue.js
+
+Vue.js is a progressive framework for building user interfaces. It can function as a library or a full-fledged framework depending on your needs.
+It primarily uses JavaScript, with support for TypeScript.
+It follows a component-based architecture similar to React and Angular. It provides a reactive and declarative approach to building UIs.
+It is suitable for both small and large applications. It’s particularly popular in the community-driven development space and for projects where quick development and ease of integration are important.
+15. Explain the concept of RESTful services and how they work.
+RESTful services, or RESTful APIs (Application Programming Interfaces), are a way to provide interoperability between computer systems on the internet. REST stands for Representational State Transfer, which is an architectural style that dictates how web services should be designed to facilitate communication and data exchange.
+
+How RESTful Services Work
+
+Client Request:
+The client sends an HTTP request to the server. The request includes the method (e.g., GET, POST), the URL identifying the resource, and potentially additional data in the request body (for POST, PUT, PATCH) or query parameters.
+Server Processing:
+The server receives the request, processes it, and interacts with the backend, such as databases or other services, to retrieve or modify the resource as needed.
+Response:
+The server sends an HTTP response back to the client. The response includes a status code indicating the result of the request (e.g., 200 OK, 201 Created, 404 Not Found), and optionally a body containing the resource representation or error message.
+16. What is the difference between GET and POST Method
+Answer: GET method is primarily utilised at the client-side to transmit a request to a particular server to get the data. GET is used for retrieving data like searching, filtering, or paging. Whereas, POST method is primarily utilised at the client-side to transmit data to a particular server. POST  is used for submitting forms, modifying data, or creating new resources.
+
+
+TCS FULL STACK INTERVIEW QUESTIONS – FREE PDF
+
+II) Behavioral Questions:
+1. Tell me about yourself.
+
+You can’t escape this question in any of the job interviews. Introducing yourself to the interview panel is generally the first thing you do in an interview. But the intention of the question is not just to know your name or educational qualification but to assess your confidence level too. In fact, this question sets the tone for the rest of the interview. Hence, this question must be very well prepared. But it must not seem rehearsed too.
+2. What are your greatest strengths?
+
+Always remember that your strengths should align with the job profile you are applying for. It can be your problem-solving skills, ability to work hard, technical expertise, leadership skills, positive attitude, etc.
+3. Where do you see yourself in five years?
+
+The tip to answering this question is to not be too specific in your reply. For example, don’t say something like you see yourself as the manager of XYZ team in the next 5 years. The intention of this question is to see if you want to get into a long-term commitment with the company.
+ 
+
+4. Describe your working style?
+
+In this question,  the interviewer wants to know your professional approach or the level of commitment to your work. They also want to see if your working style aligns with the company’s work culture. Hence, you must frame your answer accordingly. Here is an example:
+I can adapt to any workplace. While working alone, I chalk down my priorities and try to complete the tasks on schedule. But I also enjoy working in a team.
+5. Are you a team player?
+The company expects you to be a team player. So, always say this and keep some examples ready to support your answer.
+
+6. How would you be an asset to our organization?
+
+In this answer. let the company know about your strengths and how you can use the same to contribute towards the growth of the organization at large. Don’t forget! This question needs to be answered with immense confidence and conviction.
+ 
+
+7. What are your weaknesses?
+
+Refrain from saying anything that may make you seem like an unfit candidate for the job.  Some weaknesses that you can state are self-criticism, a workaholic attitude, shying away from asking for help, etc.  An example can be:
+I often tend to direct my attention towards self-improvement more than celebrating my biggest achievements in life. I know this can be helpful too, but I often fail to acknowledge myself or be satisfied with myself due to this habit. However, I am currently working on this weakness, and I am confident I will tone it down soon.
+ 
+
+8. Why do you want to work at TCS?
+
+TCS has a high rating on almost all employer rating platforms. The company is considered an employee-friendly organization and offers entry-level employees a healthy work environment and immense growth opportunities, training, and exposure in the IT industry. As a fresher or even as an experienced professional, you may state these reasons for joining TCS.
+ 
+
+9. Can you work under pressure?
+
+The ability to work under pressure is considered to be valuable quality for any professional. Hence, you must have a strong response ready to show that you are likely to perform well even under challenging circumstances.
+ 
+ FULL-STACK DEVELOPER INTERVIEW QUESTIONS AND ANSWERS
 
 Architecture
 Concurrency
